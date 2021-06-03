f5cfa08fd328ff8c1040717d84c53e0b40d2e527
Brian Thai <brian.s.thai@gmail.com>
2021-06-02 23:40:53 -0700

    initial commits for counting bits

---
+++ CountingBits/src/CountingBits.java
@@ -0,0 +1,19 @@
+package CountingBits.src;
+
+/*
+ * Given an integer n, return an array ans of length n + 1 such that for each i (0 <= i <= n), ans[i] is the number of 1's in the binary representation of i.
+ * 
+ * Example: 
+ * Input: n = 2
+ * Output: [0,1,1]
+ * Explanation:
+ * 0 --> 0
+ * 1 --> 1
+ * 2 --> 10
+*/
+public class CountingBits {
+    public int[] countBits(int n) {
+        int[] ans = new int[n+1];
+        //Need to find a pattern of numbers of 1's in binary
+    }
+}
